b40624b6fccf38425ead0c97329e82b680cea896
raehasandalwala <raehasandalwala@gmail.com>
2014-02-19 19:47:00 +0530

    added schedule content

--- src/com/iitb/aakash/nationalconference/Schedule.java
+++ src/com/iitb/aakash/nationalconference/Schedule.java
@@ -6,9 +6,12 @@ import java.util.Scanner;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.ActivityNotFoundException;
 import android.content.Intent;
@@ -17,11 +20,19 @@ import android.content.Intent;
 	public class Schedule  extends Activity {
 
 		ImageView day1,day2;
+		TextView txtHome, txtAbout, txtSchedule, txtSpeakers, txtArrangements,
+		txtContact;
 		
 		@Override
 		protected void onCreate(Bundle savedInstanceState) {
 			super.onCreate(savedInstanceState);
 			setContentView(R.layout.schedule);
+			txtHome = (TextView) findViewById(R.id.txtHome);
+			txtAbout = (TextView) findViewById(R.id.txtAbout);
+			txtSchedule = (TextView) findViewById(R.id.txtSchedule);
+			txtSpeakers = (TextView) findViewById(R.id.txtSpeaker);
+			txtArrangements = (TextView) findViewById(R.id.txtArrangement);
+			txtContact = (TextView) findViewById(R.id.txtContact);
 		
 		day1=(ImageView)findViewById(R.id.button1);
 		day2=(ImageView)findViewById(R.id.button2);
@@ -87,6 +98,74 @@ import android.content.Intent;
 			}	
 			
 		});	
+		
+	
+
+		txtAbout.setOnClickListener(new OnClickListener() {
+
+			@SuppressLint("NewApi")
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				v.setAlpha((float) 0.2);
+				Intent infoactivity = new Intent(Schedule.this, About.class);
+				startActivity(infoactivity);
+				finish();
+			}
+		});
+
+		txtHome.setOnClickListener(new OnClickListener() {
+
+			@SuppressLint("NewApi")
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				v.setAlpha((float) 0.2);
+				Intent infoactivity = new Intent(Schedule.this, Home.class);
+				startActivity(infoactivity);
+				finish();
+			}
+		});
+
+		txtSpeakers.setOnClickListener(new OnClickListener() {
+
+			@SuppressLint("NewApi")
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				v.setAlpha((float) 0.2);
+				Intent infoactivity = new Intent(Schedule.this, Speakers.class);
+				startActivity(infoactivity);
+				finish();
+			}
+		});
+
+		txtArrangements.setOnClickListener(new OnClickListener() {
+
+			@SuppressLint("NewApi")
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				v.setAlpha((float) 0.2);
+				Intent infoactivity = new Intent(Schedule.this, Arrangement.class);
+				startActivity(infoactivity);
+				finish();
+			}
+		});
+
+		txtContact.setOnClickListener(new OnClickListener() {
+
+			@SuppressLint("NewApi")
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				v.setAlpha((float) 0.2);
+				Intent infoactivity = new Intent(Schedule.this, ContactUs.class);
+				startActivity(infoactivity);
+				finish();
+			}
+		});
+
 		}
 
 		
