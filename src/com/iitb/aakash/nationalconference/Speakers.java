415fc7861fa19c6fbbb25e0c6faef80d2f1be3a0
aronnair <arunmakesarun@gmail.com>
2014-02-21 12:34:38 +0530

    Added menus on Speakers page

--- src/com/iitb/aakash/nationalconference/Speakers.java
+++ src/com/iitb/aakash/nationalconference/Speakers.java
@@ -1,22 +1,95 @@
 package com.iitb.aakash.nationalconference;
 
-import android.os.Bundle;
+import android.annotation.SuppressLint;
 import android.app.Activity;
-import android.view.Menu;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.TextView;
 
 public class Speakers extends Activity {
 
+	TextView txtHome, txtAbout, txtSchedule, txtSpeakers, txtArrangements,
+	txtContact;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.speakers);
+		
+		txtHome = (TextView) findViewById(R.id.txtHome);
+		txtAbout = (TextView) findViewById(R.id.txtAbout);
+		txtSchedule = (TextView) findViewById(R.id.txtSchedule);
+		txtSpeakers = (TextView) findViewById(R.id.txtSpeaker);
+		txtArrangements = (TextView) findViewById(R.id.txtArrangement);
+		txtContact = (TextView) findViewById(R.id.txtContact);
+		
+		txtAbout.setOnClickListener(new OnClickListener() {
+
+			@SuppressLint("NewApi")
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				v.setAlpha((float) 0.2);
+				Intent infoactivity = new Intent(Speakers.this, About.class);
+				startActivity(infoactivity);
+				finish();
+			}
+		});
+
+		txtSchedule.setOnClickListener(new OnClickListener() {
+
+			@SuppressLint("NewApi")
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				v.setAlpha((float) 0.2);
+				Intent infoactivity = new Intent(Speakers.this, Schedule.class);
+				startActivity(infoactivity);
+				finish();
+			}
+		});
+
+		txtHome.setOnClickListener(new OnClickListener() {
+
+			@SuppressLint("NewApi")
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				v.setAlpha((float) 0.2);
+				Intent infoactivity = new Intent(Speakers.this, Home.class);
+				startActivity(infoactivity);
+				finish();
+			}
+		});
+
+		txtArrangements.setOnClickListener(new OnClickListener() {
+
+			@SuppressLint("NewApi")
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				v.setAlpha((float) 0.2);
+				Intent infoactivity = new Intent(Speakers.this, Arrangement.class);
+				startActivity(infoactivity);
+				finish();
+			}
+		});
+
+		txtContact.setOnClickListener(new OnClickListener() {
+
+			@SuppressLint("NewApi")
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				v.setAlpha((float) 0.2);
+				Intent infoactivity = new Intent(Speakers.this, ContactUs.class);
+				startActivity(infoactivity);
+				finish();
+			}
+		});
 	}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.speakers, menu);
-		return true;
-	}
 
 }
